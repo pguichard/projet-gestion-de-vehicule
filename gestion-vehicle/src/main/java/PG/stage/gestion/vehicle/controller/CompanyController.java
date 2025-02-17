3e4a413e0bea83d51d2da93530f07e3563d927aa
Patrick-G <patrack17@gmail.com>
2025-02-17 17:03:16 +0100

    ajout crossorigine sur endpoint CompanyController

--- gestion-vehicle/src/main/java/PG/stage/gestion/vehicle/controller/CompanyController.java
+++ gestion-vehicle/src/main/java/PG/stage/gestion/vehicle/controller/CompanyController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/companies")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CompanyController {
 
     private final CompanyService companyService;
